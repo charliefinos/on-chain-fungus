4439aebfe79a7a80f5c4a531a2f7d3a456536669
0xcharlie <charlyfinos@gmail.com>
2023-02-10 16:19:48 -0300

    setting up

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,7 +1,9 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.17;
 
-contract Counter {
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract Fungus is Erc721 {
     uint256 public number;
 
     function setNumber(uint256 newNumber) public {
